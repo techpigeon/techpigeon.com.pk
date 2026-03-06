25ca5fd667fb0498a5a78d309a738acf1111844a
TECHPIGEON <umer@techpigeon.org>
2026-03-07 01:48:22 +0500

    deleted duplications

--- frontend/src/app/admin/layout.tsx
+++
@@ -1,4 +0,0 @@
-import AdminLayout from '@/components/layout/AdminLayout';
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <AdminLayout>{children}</AdminLayout>;
-}
