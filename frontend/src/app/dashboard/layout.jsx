6d59df8960f2da805041379fe69fa6c1b629f2c8
TECHPIGEON <umer@techpigeon.org>
2026-03-07 02:15:58 +0500

    dir removed

--- frontend/src/app/dashboard/layout.jsx
+++
@@ -1,4 +0,0 @@
-import DashboardLayout from '../../components/layout/DashboardLayout';
-export default function Layout({ children }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
--- frontend/src/components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Logo }     from './Logo';
-export { default as Button }   from './Button';
-export { default as Input }    from './Input';
-export { default as Badge }    from './Badge';
-export { default as Alert }    from './Alert';
-export { default as Modal }    from './Modal';
-export { default as Select }   from './Select';
-export { default as StatCard } from './StatCard';
-export { default as Table }    from './Table';
